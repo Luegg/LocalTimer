86bfb89ceb9d273c593607cc4373c2b7033cd8f5
Luegg <wegmaluk@gmail.com>
2012-03-21 17:15:00 +0100

    Added bootstrap css framework

--- templates/main.php
+++ templates/main.php
@@ -1,16 +1,56 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>LocalTimer</title>
-		<link href="localTimer.css" media="all" rel="stylesheet" type="text/css"/>
 		<script src="vendors/json2.js"></script>
 		<script src="vendors/jquery-1.7.1.js"></script>
 		<script src="vendors/underscore.js"></script>
 		<script src="vendors/backbone.js"></script>
 		<script src="vendors/backbone-localstorage.js"></script>
 		<script src="localTimer.js"></script>
+		<script src="vendors/bootstrap/js/bootstrap.js"></script>
+		<link href="vendors/bootstrap/css/bootstrap.css" media="all" rel="stylesheet" type="text/css"/>
+		<link href="localTimer.css" media="all" rel="stylesheet" type="text/css"/>
+		<link href="vendors/bootstrap/css/bootstrap-responsive.css" media="all" rel="stylesheet" type="text/css"/>
 	</head>
 
 	<body>
+		<div class="navbar navbar-fixed-top">
+		  <div class="navbar-inner">
+			<div class="container">
+			  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </a>
+			  <a class="brand" href="">LocalTimer</a>
+			  <div class="nav-collapse">
+				<ul class="nav">
+				  <li class="">
+					<a href="#">First</a>
+				  </li>
+				  <li class="">
+					<a href="#">Second</a>
+				  </li>
+				</ul>
+			  </div>
+			</div>
+		  </div>
+		</div>
+		<div class="container">
+		<header class="">
+			<div class="page-header">
+				<h1>Time Records <small>record your time spent for anything</small></h1>
+			</div>
+		</header>
 		
+		<section>
+			
+		</section>
+		
+		<footer>
+			Some nice blablub
+		</footer>
+		</div>
 	</body>
 </html>
